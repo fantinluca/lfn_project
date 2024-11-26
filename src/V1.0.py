7acc4af8c70244faceb8ddeec01e3a6f4a1924cb
Fabio Cociancich <fcociancich@gmail.com>
2024-11-26 12:08:44 +0100

    change directory name + code update

--- src/V1.0.py
+++ src/V1.0.py
@@ -1,17 +1,33 @@
 import pandas as pd
 import os
 import networkx as nx
+import networkit as nk
+
+
+def convert_to_networkit(nx_graph):
+    node_mapping = {node: i for i, node in enumerate(nx_graph.nodes())}
+    # Initialize an empty networkit graph
+    nk_graph = nk.graph.Graph(n=len(node_mapping), weighted=False, directed=nx_graph.is_directed())
+    
+    # Add edges from the NetworkX graph to the NetworKit graph
+    for u, v, data in nx_graph.edges(data=True):
+        weight = 1.0  # Default weight if unweighted
+        nk_graph.addEdge(node_mapping[u], node_mapping[v], weight)
+
+    return nk_graph
+
 
 # Gets the current directory where the script is located
 current_dir = os.path.dirname(__file__)
+current_dir = current_dir.replace("src", "dataset")
 
 # Builds full paths to .csv files within the current directory
 nodes_path = os.path.join(current_dir, 'nodes.csv')
 edges_path = os.path.join(current_dir, 'edges.csv')
 
 # Upload CSV files using paths relative to the current directory, limited to a subset of rows
-nodes_df = pd.read_csv(nodes_path, nrows=1000)  #limite 1.000 nodi
-edges_df = pd.read_csv(edges_path, nrows=1000)  #limite 1.000 edges
+nodes_df = pd.read_csv(nodes_path)#, nrows=1000)  #limite 1.000 nodi
+edges_df = pd.read_csv(edges_path)#, nrows=1000)  #limite 1.000 edges
 
 print("Colonne di nodes.csv:", nodes_df.columns)
 
@@ -68,3 +84,35 @@ top_eigenvector_centrality = sorted(eigenvector_centrality.items(), key=lambda x
 print("\nTop 10 Eigenvector Centrality:")
 for node, centrality in top_eigenvector_centrality:
     print(f"Node: {node}, Eigenvector Centrality: {centrality}")
+
+
+# LOCAL clustering coefficients
+G_nk = convert_to_networkit(G)
+#clustCoeff = nk.globals.ClusteringCoefficient.exactGlobal(G_nk)
+clustCoeff = nk.centrality.LocalClusteringCoefficient(G_nk)                 # Local clustering coefficient
+# Run the algorithm to compute the coefficients
+clustCoeff.run()
+
+# Retrieve the clustering coefficients for each node
+clustering_coefficients = clustCoeff.scores()
+
+# Print the coefficients for each node
+#for node, coeff in enumerate(clustering_coefficients):
+#    print(f"Node {node}: Clustering Coefficient = {coeff}")
+top_clustCoeff = sorted(clustering_coefficients, key=lambda x: x, reverse=True)[:10]
+list_top_coeff_pairs = list(enumerate(top_clustCoeff))
+print("\nTop 10 Local clustering Coefficients:")
+for node, coeff in enumerate(list_top_coeff_pairs):
+    print(f"Node {node}: Clustering Coefficient = {coeff}")
+
+
+# GLOBAL CLUSTERING COEFF
+globClustCoeff = nk.globals.ClusteringCoefficient.exactGlobal(G_nk)
+print ("Global cc = ", globClustCoeff )
+
+
+# APPROX GLOBAL CLUSTERING COEFF
+globClustCoeffApprox = nk.globals.ClusteringCoefficient.approxGlobal(G_nk, trials = 10000)
+print ("Approx global cc = ", globClustCoeffApprox )
+
+
