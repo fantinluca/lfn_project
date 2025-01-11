a04e8f18ba940984e69edd6a2ae0e5e5d18af964
ferrarif1w <95565152+ferrarif1w@users.noreply.github.com>
2025-01-11 10:24:31 +0100

    various changes, verified cc hp

--- src/old_code/check_cc_hp.py
+++ src/old_code/check_cc_hp.py
@@ -18,7 +18,7 @@ nodes_df, _ = create_graphs.read_dataset()
 nodes_df = nodes_df.rename(columns={"spotify_id": "node"})
 nodes_df = nodes_df.set_index("node")
 
-df = pd.read_csv("../results/real_node_metrics_clustering_coeffs.csv", sep=';', index_col=0)
+df = pd.read_csv(os.path.join(result_dir, "real", "real_node_metrics_clustering_coeffs.csv"), sep=';', index_col=0)
 df = df.join(nodes_df["name"], lsuffix="_metrics", rsuffix="_dataset")
 df = df.sort_values("clustering_coeffs", ascending=False)
 
@@ -26,13 +26,10 @@ cc1 = df.index[df["clustering_coeffs"]==1.0].to_list()
 not_verified_count = 0
 for node in cc1:
     hp_verified = True
-    con_comp = nx.node_connected_component(G, node)
-    for node_pair in it.combinations(con_comp, 2):
-        if not (G.has_edge(node, node_pair[0]) and G.has_edge(node, node_pair[1]) and G.has_edge(node_pair[0], node_pair[1])):
-            hp_verified = False
-            break
-    
-    if not hp_verified: not_verified_count += 1
-    print(f"Hypothesis {'not' if not hp_verified else ''} verified for artist {df.loc[node]['name']}")
+    neigh = nx.neighbors(G, node)
+    for node_pair in it.combinations(neigh, 2):
+        if not G.has_edge(node_pair[0], node_pair[1]):
+            not_verified_count += 1
+            print(f"Hypothesis not verified for artist {df.loc[node]['name']}")
 
 print(f"In the end, hypothesis not verified for {not_verified_count} artists out of {len(cc1)} artists")
